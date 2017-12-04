315dda232af81262fc06b2540fca685365544187
duskcloudxu <duskcloudxu@gmail.com>
2017-12-04 23:16:05 +0800

    searchRes画面Iphone6S plus适配完成

--- app/js/controller.js
+++ app/js/controller.js
@@ -14,7 +14,9 @@ const app = angular.module('app.controller', [])
     })
 
     .controller('searchResCtrl', function ($scope) {
-
+        $scope.records = [
+            1,2,3,4,5,6,7,8,9,89,10,11,12
+        ]
     })
 
     .controller('checkOrderCtrl', function ($scope) {
